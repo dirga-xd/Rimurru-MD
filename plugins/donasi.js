b545108fc983f30d5d29826a0c2930c931f43104
dirga-xd <108272944+dirga-xd@users.noreply.github.com>
2022-08-22 12:31:15 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,15 @@
 let handler = async m => m.reply(`
-╭─「 Donasi 」
-│ •  [0838-2112-3163]
-│ •  [trakteer.id/xlaw_xd]
+*Mau donasi bang?  Silahkan donasi seikhlasnya aja buat beli kuota😉*
+
+╭─「 Donasi • Pulsa 」
+│ • Telkomnyet [0812-8277-8090]
+│ • Axis [0838-2112-3163]
 ╰────
+
+╭─「 Donasi • Non Pulsa 」
+│ • OVO, Dana [0838-2112-3163]
+│ • https://trakteer.id/xlaw_xd
+╰────Io
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']

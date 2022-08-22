6d1e89d6f6daaf47e1bb6c23b428708a06350a26
dirga-xd <108272944+dirga-xd@users.noreply.github.com>
2022-08-22 12:03:40 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi 」
+│ •  [0838-2112-3163]
+│ •  [trakteer.id/xlaw_xd]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
